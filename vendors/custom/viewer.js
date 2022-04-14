0d6af295488c3144c1309349c94dc82c5d95ca24
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2022-04-14 23:56:16 +0200

    refresh delay

--- vendors/custom/viewer.js
+++ vendors/custom/viewer.js
@@ -1,3 +1,4 @@
+// supress all output from pdfjs
 console.log   = () => {}
 console.error = () => {}
 console.warn  = () => {}
@@ -13,15 +14,47 @@ window.addEventListener("message", event => {
   const data = event.data.data;
   switch (type) {
     case "refresh":
-      refreshContents(data.filepath);
-      return;
+      return refreshContents(data.filePath);
     case "setposition":
-      scrollToPosition(data);
-      return;
+      return scrollToPosition(data);
     default:
       throw new Error(`Unexpected message type "${type}" received`);
   }
 });
+
+function refreshContents(filePath) {
+  if (typeof filePath !== "string") {
+    throw new Error(`Expected string as filepath, got ${filePath}`);
+  }
+  if (window.frameElement && window.frameElement.style.display === "none") {
+    // we are not in view; don't bother updating
+    lastFilepath = filePath;
+    styleObserver.observe(window.frameElement, {
+      attributes: true,
+      attributeFilter: ["style"],
+    });
+    return;
+  }
+  lastParams = getDocumentParams() || lastParams;
+  PDFViewerApplication.open(filePath);
+  document.addEventListener("pagesinit",() => {restoreFromParams(lastParams)},{once: true, passive: true}
+  );
+}
+
+function getDocumentParams() {
+  const container = document.getElementById("viewerContainer");
+  const params = {
+    scale: PDFViewerApplication.pdfViewer.currentScaleValue,
+    scrollTop: container.scrollTop,
+    scrollLeft: container.scrollLeft,
+  };
+  // When the PDF is incomplete (e.g., long rebuild), scale will be null. Probably.
+  if (params.scale !== null) {
+    return params;
+  } else {
+    return undefined;
+  }
+}
 
 function scrollToPosition({pageIndex, pointX, pointY, origin}) {
   const pageView = PDFViewerApplication.pdfViewer.getPageView(pageIndex);
