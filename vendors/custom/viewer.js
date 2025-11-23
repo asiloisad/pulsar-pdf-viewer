9a2d8c9aa44657adca84af393da8b58559cb2861
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2025-11-24 00:00:06 +0100

    update reload

--- vendors/custom/viewer.js
+++ vendors/custom/viewer.js
@@ -1,5 +1,5 @@
 // supress all output from pdfjs
-console.log = console.error = console.warn = () => {}
+console.log = console.error = console.warn = () => { }
 
 window.onload = () => {
   PDFViewerApplicationOptions.set("sidebarViewOnLoad", 0)
@@ -8,40 +8,41 @@ window.onload = () => {
   PDFViewerApplicationOptions.set("externalLinkTarget", 4)
   PDFViewerApplicationOptions.set("isEvalSupported", false)
   PDFViewerApplicationOptions.set("disableHistory", true)
+  parent.postMessage({ type: 'ready' })
 }
 
 window.addEventListener('keydown', (event) => {
-  if (event.keyCode===119) {
+  if (event.keyCode === 119) {
     return toggleInvertMode()
-  } else if (event.ctrlKey && event.keyCode===116) {
-    return parent.postMessage({type:'keydown', action:'toggle-refreshing'})
-  } else if (event.keyCode===116) {
-    return refreshContents({filePath:PDFViewerApplication.url})
-  } else if (event.keyCode===112) {
-    return parent.postMessage({type:'keydown', action:'command-palette:toggle'})
-  } else if (event.altKey && event.keyCode===78) {
-    return parent.postMessage({type:'keydown', action:'navigation-panel:toggle'})
-  } else if (event.altKey && event.keyCode===123) {
-    return parent.postMessage({type:'keydown', action:'open-external:open'})
-  } else if (event.ctrlKey && event.keyCode===123) {
-    return parent.postMessage({type:'keydown', action:'open-external:show'})
-  } else if (event.altKey && event.keyCode===121) {
+  } else if (event.ctrlKey && event.keyCode === 116) {
+    return parent.postMessage({ type: 'keydown', action: 'toggle-refreshing' })
+  } else if (event.keyCode === 116) {
+    return refreshContents({ filePath: PDFViewerApplication.url })
+  } else if (event.keyCode === 112) {
+    return parent.postMessage({ type: 'keydown', action: 'command-palette:toggle' })
+  } else if (event.altKey && event.keyCode === 78) {
+    return parent.postMessage({ type: 'keydown', action: 'navigation-panel:toggle' })
+  } else if (event.altKey && event.keyCode === 123) {
+    return parent.postMessage({ type: 'keydown', action: 'open-external:open' })
+  } else if (event.ctrlKey && event.keyCode === 123) {
+    return parent.postMessage({ type: 'keydown', action: 'open-external:show' })
+  } else if (event.altKey && event.keyCode === 121) {
     // return parent.postMessage({type:'keydown', action:'project-list:recent'})
-  } else if (event.keyCode===121) {
+  } else if (event.keyCode === 121) {
     // return parent.postMessage({type:'keydown', action:'project-list:toggle'})
-  } else if (event.altKey && event.keyCode===37) {
-    return parent.postMessage({type:'keydown', action:'window:focus-pane-on-left'})
-  } else if (event.altKey && event.keyCode===38) {
-    return parent.postMessage({type:'keydown', action:'window:focus-pane-above'})
-  } else if (event.altKey && event.keyCode===39) {
-    return parent.postMessage({type:'keydown', action:'window:focus-pane-on-right'})
-  } else if (event.altKey && event.keyCode===40) {
-    return parent.postMessage({type:'keydown', action:'window:focus-pane-below'})
+  } else if (event.altKey && event.keyCode === 37) {
+    return parent.postMessage({ type: 'keydown', action: 'window:focus-pane-on-left' })
+  } else if (event.altKey && event.keyCode === 38) {
+    return parent.postMessage({ type: 'keydown', action: 'window:focus-pane-above' })
+  } else if (event.altKey && event.keyCode === 39) {
+    return parent.postMessage({ type: 'keydown', action: 'window:focus-pane-on-right' })
+  } else if (event.altKey && event.keyCode === 40) {
+    return parent.postMessage({ type: 'keydown', action: 'window:focus-pane-below' })
   }
 })
 
 window.addEventListener('click', () => {
-  parent.postMessage({type:'click'})
+  parent.postMessage({ type: 'click' })
 })
 
 window.addEventListener('contextmenu', (event) => {
@@ -72,39 +73,39 @@ window.addEventListener('contextmenu', (event) => {
   const res = PDFViewerApplication.pdfViewer.currentScale * 96
   x = Math.round(x / res * 72)
   y = Math.round(y / res * 72)
-  parent.postMessage({type:'contextmenu', pageNo:pageNo, x:x, y:y})
+  parent.postMessage({ type: 'contextmenu', pageNo: pageNo, x: x, y: y })
 })
 
 window.addEventListener("message", (message) => {
-  if (message.source!==parent) {
+  if (message.source !== parent) {
     return
-  } else if (message.data.type==='refresh') {
+  } else if (message.data.type === 'refresh') {
     return refreshContents(message.data);
-  } else if (message.data.type==='setposition') {
+  } else if (message.data.type === 'setposition') {
     return scrollToPosition(message.data);
-  } else if (message.data.type==='setdestination') {
+  } else if (message.data.type === 'setdestination') {
     return scrollToDestination(message.data);
-  } else if (message.data.type==='invert') {
+  } else if (message.data.type === 'invert') {
     return toggleInvertMode(message.data)
-  } else if (message.data.type==='currentdest') {
+  } else if (message.data.type === 'currentdest') {
     return spawnCurrentDest(message.data)
   }
 })
 
-let lastParams = { page:1, zoom:'auto' }
+let lastParams = { page: 1, zoom: 'auto' }
 
 function refreshContents(data) {
-  if (window.frameElement && window.frameElement.style.display==="none") {
+  if (window.frameElement && window.frameElement.style.display === "none") {
     return
-  } else if (PDFViewerApplication.pagesCount>1) {
+  } else if (PDFViewerApplication.pagesCount > 1) {
     lastParams.page = PDFViewerApplication.page
     lastParams.zoom = PDFViewerApplication.pdfViewer.currentScaleValue
     if (/^\d+(?:\.\d+)?$/.test(lastParams.zoom)) {
-      lastParams.zoom = parseFloat(lastParams.zoom)*100
+      lastParams.zoom = parseFloat(lastParams.zoom) * 100
     }
   }
   PDFViewerApplication.initialBookmark = `page=${lastParams.page}&zoom=${lastParams.zoom}`;
-  PDFViewerApplication.open({ url:data.filePath })
+  PDFViewerApplication.open({ url: data.filePath })
 }
 
 function scrollToPosition(data) {
@@ -112,8 +113,8 @@ function scrollToPosition(data) {
   const clientHeight = PDFViewerApplication.appConfig.mainContainer.clientHeight;
   const clientWidth = PDFViewerApplication.appConfig.mainContainer.clientWidth;
   const height = pageView.div.offsetTop;
-  const [, y1,, y2] = pageView.viewport.viewBox;
-  const [x, y] = pageView.viewport.convertToViewportPoint(data.x, y2-y1-data.y);
+  const [, y1, , y2] = pageView.viewport.viewBox;
+  const [x, y] = pageView.viewport.convertToViewportPoint(data.x, y2 - y1 - data.y);
   const percentDown = 0.50; const percentAcross = 0.50;
   PDFViewerApplication.pdfViewer.container.scrollTo({
     top: height + y - clientHeight * percentDown,
